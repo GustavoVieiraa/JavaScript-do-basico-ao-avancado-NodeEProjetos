3c705932873034115dea25492a095b96ade5ab41
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-27 13:09:06 -0300

    Tipo de dado - string - concluído com êxito.
    Tipo de dado - string - concluído com êxito.

---
+++ Tipos de dados e operadores lógicos/Tipo de dado - string/script.js
@@ -0,0 +1,7 @@
+console.log("Oi, meu nome é Gustavo");
+console.log('Esse é um teste para strings');
+console.log(`Esse é o teste no navegador`);
+
+console.log(typeof "Oi, meu nome é Gustavo");
+console.log(typeof 'Esse é um teste para strings');
+console.log(typeof `Esse é o teste no navegador`);
