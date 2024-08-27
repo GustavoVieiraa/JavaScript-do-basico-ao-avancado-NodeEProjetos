079ba3c2740cc45275125fe867b46a46f3045977
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-27 13:01:44 -0300

    Operações aritméticas com number - concluído com êxito.
    Operações aritméticas com number - concluído com êxito.

---
+++ Tipos de dados e operadores lógicos/Operações aritméticas com number/script.js
@@ -0,0 +1,11 @@
+console.log(2 + 3);
+console.log(5 - 12);
+console.log(5 * 20);
+console.log(5 / 3);
+console.log(5 % 2);
+
+console.log(typeof (2 + 3));
+console.log(typeof (5 - 12));
+console.log(typeof (5 * 20));
+console.log(typeof (5 / 3));
+console.log(typeof (5 % 2));
