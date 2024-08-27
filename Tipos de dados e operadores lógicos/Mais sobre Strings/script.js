efb033c25bac111995f1611021e3a08648dca847
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-27 13:18:53 -0300

    Mais sobre Strings - concluído com êxito.
    Mais sobre Strings - concluído com êxito.

---
+++ Tipos de dados e operadores lógicos/Mais sobre Strings/script.js
@@ -0,0 +1,13 @@
+console.log("Oi, meu nome é Gustavo");
+console.log('Esse é um teste para strings');
+console.log(`Esse é o teste no navegador`);
+
+console.log(typeof "Oi, meu nome é Gustavo");
+console.log(typeof 'Esse é um teste para strings');
+console.log(typeof `Esse é o teste no navegador`);
+
+console.log("Primeira linha \n Segunda linha");
+console.log("Meu nome é 'Gustavo Vieira'");
+console.log('Meu nome é "Gustavo Vieira"');
+
+console.log(`A multiplicação de 5 por 5 é ${5 * 5}`);
