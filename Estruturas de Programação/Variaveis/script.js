379e3cfee6f363a0c1f101c429e80c72795e9d5a
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-28 08:39:54 -0300

    Variaveis - concluído com êxito.
    Variaveis - concluído com êxito.

---
+++ Estruturas de Programação/Variaveis/script.js
@@ -0,0 +1,20 @@
+let nome = "Gustavo";
+
+console.log(nome);
+console.log(`Meu nome é ${nome}`);
+
+let laranjas = 5;
+
+console.log(laranjas * laranjas);
+
+nome = "João";
+
+console.log(nome);
+
+laranja = 329312;
+
+console.log(laranja);
+
+laranja = "Laranja";
+
+console.log(laranja);
