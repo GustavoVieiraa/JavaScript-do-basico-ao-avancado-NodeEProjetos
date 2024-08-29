98cdb02c3454c46219c2418bb765b06bf357935a
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-29 07:36:48 -0300

    Estrutura de Controle - Else If - concluído com êxito.
    Estrutura de Controle - Else If - concluído com êxito.

---
+++ Estruturas de Programação/Estrutura de Controle - Else If/script.js
@@ -0,0 +1,9 @@
+let nome = "Gustavo";
+
+if (nome != undefined) {
+    console.log("Nome está definido");
+} else if (nome == "Rodolfo" && nome.length > 5) {
+    console.log("O nome é Rodolfo");
+} else {
+    console.log("Não é o Rodolfo");
+}
