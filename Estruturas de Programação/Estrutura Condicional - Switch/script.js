5083fc6878e019d5b66afd05c2f740577b7858a2
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-29 07:59:17 -0300

    Estrutura Condicional - Switch - concluído com êxito.
    Estrutura Condicional - Switch - concluído com êxito.

---
+++ Estruturas de Programação/Estrutura Condicional - Switch/script.js
@@ -0,0 +1,15 @@
+let nome = "Gustavo";
+
+switch (nome) {
+    case "Gabriel":
+        console.log("O seu nome é Gabriel");
+        break;
+    case "Marcos":
+        console.log("O seu nome é Marcos");
+        break;
+    case "Gustavo":
+        console.log("O seu nome é Gustavo");
+        break;
+    default: 
+        console.log("O seu nome não foi encontrado");
+}
