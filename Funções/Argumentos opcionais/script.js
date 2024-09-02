90c2d9d9bae5a49207aa1db522a87559c44f8779
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-09-02 10:07:30 -0300

    Argumentos opcionais - concluído com êxito.
    Argumentos opcionais - concluído com êxito.

---
+++ Funções/Argumentos opcionais/script.js
@@ -0,0 +1,9 @@
+function soma(a, b) {
+    if (a === undefined || b === undefined) {
+        console.log("Está função precisa ter dois argumentos.");
+    } else {
+        return a + b;
+    }
+}
+
+soma(1);
