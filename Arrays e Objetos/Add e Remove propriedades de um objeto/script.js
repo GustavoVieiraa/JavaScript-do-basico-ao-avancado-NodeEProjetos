79de2deab228d411df0d7fc5e18e97bdb34aba6a
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-09-02 12:53:54 -0300

    Add e Remove propriedades de um objeto - concluído com êxito.
    Add e Remove propriedades de um objeto - concluído com êxito.

---
+++ Arrays e Objetos/Add e Remove propriedades de um objeto/script.js
@@ -0,0 +1,13 @@
+let pessoa = {
+    nome: "Gustavo",
+    idade: 21,
+    profissao: "Programador FullStack"
+};
+
+console.log(pessoa.nome);
+
+delete pessoa.nome;
+
+pessoa.relacionamento = true;
+
+console.log(pessoa);
