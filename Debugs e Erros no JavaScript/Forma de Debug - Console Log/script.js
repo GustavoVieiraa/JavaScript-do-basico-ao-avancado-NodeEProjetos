5d4204eeff5e81c7a215572708a9a9743eb02efa
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-09-03 09:05:39 -0300

    Forma de Debug - Console Log - concluído com êxito.
    Forma de Debug - Console Log - concluído com êxito.

---
+++ Debugs e Erros no JavaScript/Forma de Debug - Console Log/script.js
@@ -0,0 +1,25 @@
+let a = 1;
+let b = 2; 
+let c = 3;
+
+if (c > a) {
+    a = b;
+    console.log(a);
+}
+
+for(let i = 5; i > 0; i--) {
+    b++;
+    a = a + c + 1;
+    c += 2;
+    console.log(a);
+}
+
+if (b == a) {
+    a++;
+    console.log(a);
+} else {
+    a = a + b + c;
+}
+
+a = a * b;
+console.log(a);
