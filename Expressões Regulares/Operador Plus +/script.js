ef6577a799e111dea60cc931eff4b0d75bb20a1b
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-09-04 06:50:11 -0300

    Operador Plus + - concluído com êxito.
    Operador Plus + - concluído com êxito.

---
+++ Expressões Regulares/Operador Plus +/script.js
@@ -0,0 +1,7 @@
+const umOuMaisNumeros = /\d+/;
+
+console.log(umOuMaisNumeros.test("1518"));
+console.log(umOuMaisNumeros.test(""));
+console.log(umOuMaisNumeros.test("asdasdsadsa"));
+console.log(umOuMaisNumeros.test("1"));
+console.log(umOuMaisNumeros.test("15151515151522232"));
