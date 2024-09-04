940d99b325cbbe40a20dc7c1d7676394b7ba86e7
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-09-04 00:07:17 -0300

    Caracteres Especiais - concluído com êxito.
    Caracteres Especiais - concluído com êxito.

---
+++ Expressões Regulares/Caracteres Especiais/script.js
@@ -0,0 +1,19 @@
+const pontoRegex = /./;
+console.log(pontoRegex.test("asd"));
+console.log(pontoRegex.test(" "));
+console.log(pontoRegex.test("123"));
+console.log(pontoRegex.test("sad2321"));
+
+const dRegex = /\d/; // [0-9];
+console.log('d');
+console.log(dRegex.test("asd"));
+console.log(dRegex.test(" "));
+console.log(dRegex.test("123"));
+console.log(dRegex.test("sdw2312"));
+
+const dRegex2 = /\D/; // [^0-9];
+console.log("D");
+console.log(dRegex2.test("asd"));
+console.log(dRegex2.test(" "));
+console.log(dRegex2.test("123"));
+console.log(dRegex2.test("sdw2312"));
